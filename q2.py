15e5356b01d1e54d85ba443c7c21366217e683ae
kasternz <nicolas.zarfino@uoit.net>
2017-01-18 12:08:54 -0500

    Update q2.py

--- q2.py
+++ q2.py
@@ -2,7 +2,7 @@ inputname = raw_input("Enter name: ")
 print  inputname
 inputage = input("Enter age: ")
 print  inputage
-if inputage >17:
+if inputage >18:
    print "you are allowed to drive"
-elif inputage <= 17:
+elif inputage <= 18:
    print "you are not allowed to drive"
