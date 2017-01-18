9e27f5f3b23386c12c679d8a2b151ad35f1f8c48
kasternz <nicolas.zarfino@uoit.net>
2017-01-18 10:23:47 -0500

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,7 @@
+#question 1
+x = 10
+print "I received: %d" %x
+value1 = 2 ** 12
+print value1
+value2 = 1.33 * 2
+print round(value2, 1)
---
+++ q2.py
@@ -0,0 +1,8 @@
+inputname = raw_input("Enter name: ")
+print  inputname
+inputage = input("Enter age: ")
+print  inputage
+if inputage >17:
+   print "you are allowed to drive"
+elif inputage <= 18:
+   print "you are not allowed to drive"
