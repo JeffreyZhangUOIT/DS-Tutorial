c491e5dc1aa7484de4d92baefbd592ae27c6f92e
kasternz <nicolas.zarfino@uoit.net>
2017-01-18 10:47:28 -0500

    Update q2.py

--- q2.py
+++ q2.py
@@ -4,5 +4,5 @@ inputage = input("Enter age: ")
 print  inputage
 if inputage >17:
    print "you are allowed to drive"
-elif inputage <= 18:
+elif inputage <= 17:
    print "you are not allowed to drive"
