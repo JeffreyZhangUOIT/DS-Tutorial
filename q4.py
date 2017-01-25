41e721ef0cb2b37bbc5481847157c7d7c171247f
Justin K -Linux <justinjoseph0007@gmail.com>
2017-01-25 09:56:30 -0500

    corrected the file name

--- q4.py
+++ q4.py
@@ -1,9 +1,8 @@
-file = open("text.txt", "r")
+file = open("input.txt", "r")
 for line in file:
     if "teaching assistant" in line:
         pos = line.find("teaching assistant")
         print line[:(pos+18)] + " is awesome" + line[(pos+18):]
     else:
         print line
-
 file.close()
