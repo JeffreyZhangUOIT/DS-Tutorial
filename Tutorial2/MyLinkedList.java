5895238b4c68780ae8a05eea4e9b65782c3f9868
Justin K -Linux <justinjoseph0007@gmail.com>
2017-01-31 21:38:37 -0500

    intial commit with some code

---
+++ Tutorial2/MyLinkedList.java
@@ -0,0 +1,29 @@
+class MyLinkedList extends Node
+{
+    String value;
+    Node pointer;
+
+    public MyLinkedList(String data)
+	{
+        this.value = data;
+        this.pointer = null;
+    }
+    public static void add(){
+        
+    }
+    public static void remove(){
+        
+    }
+    public static viod add(int index){
+        
+    }
+    public static void remove(int index){
+        
+    }
+    public static MyLinkedList next(){
+        
+    }
+    public static void main(String[] args){
+        ;
+    }
+}
