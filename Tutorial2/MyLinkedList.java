3287293d8003cecd94a5b62c1a2023fa164d5ecf
Justin K -Linux <justinjoseph0007@gmail.com>
2017-02-01 10:32:17 -0500

    stupid commit

--- Tutorial2/MyLinkedList.java
+++ Tutorial2/MyLinkedList.java
@@ -1,29 +1,51 @@
 class MyLinkedList extends Node
 {
-    String value;
-    Node pointer;
+    // String value;
+    public int counter;
+    private Node pointer;
 
     public MyLinkedList(String data)
 	{
-        this.value = data;
+        // this.value = data;
         this.pointer = null;
     }
-    public static void add(){
+
+    public static void add(String data)
+    {
+        if(pointer == null)
+            pointer = new Node(data);
+
+        Node temp = new Node(data);
+        Node current = pointer;
+
+        if(current != null)
+            {
+                while(current.getNext() != null)
+                    current = current.getNext();
+
+                current.setNext(temp);
+            }
+        counter++;
+    }
+
+    public static void remove()
+    {
         
     }
-    public static void remove(){
+    public static viod add(int index)
+    {
         
     }
-    public static viod add(int index){
+    public static void remove(int index)
+    {
         
     }
-    public static void remove(int index){
+    public static MyLinkedList next()
+    {
         
     }
-    public static MyLinkedList next(){
-        
-    }
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
         ;
     }
 }
